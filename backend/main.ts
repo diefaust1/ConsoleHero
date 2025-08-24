0925f4fc6d3e186d24b01f415464063a772f4906
Eric <ericfaust@web.de>
2025-08-24 16:16:18 +0200

    added connection

--- backend/main.ts
+++ backend/main.ts
@@ -1,7 +1,20 @@
 
-function handler(): Response {
-  return new Response("It's happening!");
+function handler(req: Request): Response {
+  
+  const headers = new Headers({
+    "Access-Control-Allow-Origin": "*",        // or your frontend origin
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  });
+
+  console.log("Method: " + req.method);
+  console.log("URL: " + req.url);
+  
+  if(req.method == "POST"){
+    console.log("Post success");
+    return new Response("Send Data", {headers});
+  }
+  return new Response("GET Request", {headers});
 }
 
-Deno.serve(handler);
-
+Deno.serve({ port: 4000 },handler);
--- frontend/scripts/index_script.js
+++ frontend/scripts/index_script.js
@@ -1,17 +1,27 @@
 const incrementButton = document.getElementById("increaseButton");
-const saveButton = document.getElementById("saveButton")
+const saveButton = document.getElementById("saveButton");
+const fetchButton = document.getElementById("fetchButton");
 const number = document.getElementById("number");
 let count = 0;
 
-incrementButton.addEventListener("click", increaseNumber)
-saveButton.addEventListener("click", save)
+incrementButton.addEventListener("click", increaseNumber);
+saveButton.addEventListener("click", save);
+fetchButton.addEventListener("click", testFetch);
+
+function testFetch(){
+    console.log("fetch");
+    fetch("http://localhost:4000", 
+        {method: "GET"})
+        .then(res => res.text())
+        .then(data => console.log(data));
+};
 
 
 function increaseNumber() {
     count++;
     number.textContent = count;
-}
+};
 
 function save() {
-    console.log(count)
-}
+    console.log(count);
+};
